e8c2d50c3bbd9dc7ff224c3fcecc3c45a74b6c59
Oisin Stapleton <G00365445@atu.ie>
2025-09-29 09:38:32 +0100

    Added new methods multiply and divide. Division by zero condition added.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,26 @@
 package org.example;
 public class Calculator {
+
+    // Add two numbers
     public int add(int firstNum, int secondNum) {
         return firstNum + secondNum;
     }
-    //Used for adding, subtracting, multiplying & dividing nums later
+
+    // Subtract two numbers
+    public int subtract(int firstNum, int secondNum) {
+        return firstNum - secondNum;
+    }
+
+    // Multiply two numbers
+    public int multiply(int firstNum, int secondNum) {
+        return firstNum * secondNum;
+    }
+
+    // Divide two numbers
+    public int divide(int firstNum, int secondNum) {
+        if (secondNum == 0) {
+            System.out.println("Cannot divide by zero");
+        }
+        return firstNum / secondNum;
+    }
 }
