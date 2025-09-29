7d12269dfb02d2758fd015c392a81af529f949ba
Oisin Stapleton <G00365445@atu.ie>
2025-09-29 09:46:04 +0100

    Added operation selection

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class MainApp {
     public static void main(String[] args)
     {
+
+        //setup scanner
         Scanner scan = new Scanner(System.in);
+
+        //get operation from user
+        System.out.println("Please enter an operation (add, subtract, multiply, divide)");
+        String operation = scan.next();
+        System.out.println(operation);
+
         System.out.println("Please enter your first number");
         int firstNum = scan.nextInt();
         System.out.println("Please enter your second number");
@@ -13,6 +21,7 @@ public class MainApp {
         calc.add(firstNum, secondNum);
         int result = calc.add(firstNum, secondNum);
         System.out.println("The result is: " + result);
+
 
     }
 }
